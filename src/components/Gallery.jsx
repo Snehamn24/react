18de567c14ebef8ddbd7a885b6ba58d4dc9e7487
Snehamn24 <snehamn216@gmail.com>
2026-01-14 21:23:22 +0530

    Learnt the how to add images in different way

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,25 @@
+import wall from "../assets/dark_laptop.jpg"
+import "./Gallery.css"
 export default function Gallery()
 {
     return(
+        <>
+  
+        <div>
         <img src="../dark_laptop.jpg"
         alt="Image"
+        className="main"
+        />
+        <p>Gallerry</p>
+        </div>
+        <div>
+            <img src={wall}
+        alt="second"
         width={500}
-        height={600}/>
+        height={1000}/>
+        </div>
+        
+    </>
+        
     )
 }
