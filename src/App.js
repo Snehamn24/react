999c34f491de4fcadae5815311a9a99f00fd89ee
Snehamn24 <snehamn216@gmail.com>
2026-01-14 23:37:54 +0530

    Learnt component composition root,parent and child components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Gallery from './components/Gallery';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Gallery />
+      <Home />
       
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import Gallery from "./Gallery"
+export default function Home()
+{
+    return(
+        <section id="gallery">
+            <Gallery />
+
+        </section>
+    )
+}
