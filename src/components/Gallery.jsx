a98f99f2d97006dc303efd3b188c553af273609f
Snehamn24 <snehamn216@gmail.com>
2026-01-14 23:18:16 +0530

    Tailwind Css Configured

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,12 @@
 import wall from "../assets/dark_laptop.jpg"
 import "./Gallery.css"
+
+import { useState } from "react"
+
 export default function Gallery()
 {
+    const name = "MERN"
+    const [ count,setCount ] = useState(0)
     return(
         <>
   
@@ -13,6 +18,12 @@ export default function Gallery()
         onClick={()=>alert("Hii")}
         />
         <p>Gallerry</p>
+        <h1>{name}</h1>
+        <button className="text-xl text-blue-500 bg-black" onClick={()=>setCount(count+1)}>Increment+</button>
+        <br></br>
+        <br></br>
+        <button className="text-3xl text-pink-600" onClick={()=>setCount(count-1)}>Decrement-</button>
+        <h1>{count}</h1>
         </div>
         
             <img src={wall}
